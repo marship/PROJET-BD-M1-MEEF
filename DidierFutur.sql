4c898a079c6001258ed98c458e083d9b36764fa2
Paul Faure-Jeunot <61384536+marship@users.noreply.github.com>
2022-10-23 09:41:15 +0200

    MAJ BD

--- DidierFutur.sql
+++ DidierFutur.sql
@@ -2,7 +2,6 @@ DROP TABLE FILM;
 DROP TABLE REALISATEUR;
 DROP TABLE ACTEUR;
 DROP TABLE JOUER;
-DROP TABLE GENRE;
 DROP TABLE CATEGORIE;
 DROP TABLE CARTE_BANCAIRE; 
 DROP TABLE CLIENT;
@@ -13,128 +12,113 @@ DROP TABLE CARTE_ABONNEMENT;
 DROP TABLE HISTORIQUE; 
 
 CREATE TABLE REALISATEUR (
-    IdRealisateur number(4),
-    NomRealisateur varchar2(100) not null,
+    NomRealisateur varchar2(100),
     PrenomRealisateur varchar2(100) not null,
     DateNaissanceRealisateur date,
     NationnaliteRealisateur varchar2(100),
-    constraint REALISATEUR_C1_PK primary key (IdRealisateur)
+    constraint REALISATEUR_C1_PK primary key (NomRealisateur)
 );
 
 CREATE TABLE ACTEUR (
-    IdActeur number(4),
-    NomActeur varchar2(100) not null,
+    NomActeur varchar2(100),
     PrenomActeur varchar2(100) not null,
     DateNaissanceActeur date,
     NationnaliteActeur varchar2(100),
-    constraint ACTEUR_C1_PK primary key (IdActeur)
+    constraint ACTEUR_C1_PK primary key (NomActeur)
 );
 
 CREATE TABLE FILM (
-	IdFilm number(4),
-	NomFilm varchar2(100) not null,
+	NomFilm varchar2(100),
 	ResumeFilm varchar2(1000),
     DateSortie date,
-    DureeFilm time,
+    DureeFilm date,
     NombreExemplaireFilm number(3) not null,
     LimiteAgeFilm number(2) DEFAULT 0,
     DateAjoutFilm date,
-    NombreTotalLocationFilm number(1000) DEFAULT 0,
-    IdRealisateur number(4),
-	constraint FILM_C1_PK primary key (idFilm),
+    NombreTotalLocationFilm number(10) DEFAULT 0,
+    NomRealisateur varchar2(100),
+	constraint FILM_C1_PK primary key (NomFilm),
     constraint FILM_C2_NB_EXEMPLAIRES check (NombreExemplaireFilm between 1 and 101),
-    constraint FILM_C3_FK1 foreign key (IdRealisateur) references REALISATEUR (IdRealisateur)
+    constraint FILM_C3_FK1 foreign key (NomRealisateur) references REALISATEUR (NomRealisateur)
 );
 
 CREATE TABLE JOUER (
-    IdFilm number(4),
-    IdActeur number(4),
-    constraint JOUER_C1_PK primary key (IdFilm, IdActeur),
-    constraint JOUER_C2_FK1 foreign key (IdFilm) references FILM (IdFilm),
-    constraint JOUER_C3_FK2 foreign key (IdActeur) references ACTEUR (IdActeur)
-);
-
-CREATE TABLE GENRE (
-    IdGenre number(4),
-    NomGenre varchar2(100) not null,
-    DescriptionGenre varchar2(100),
-    constraint GENRE_C1_PK primary key (IdGenre)
+    NomFilm varchar2(100),
+    NomActeur varchar2(100),
+    constraint JOUER_C1_PK primary key (NomFilm, NomActeur),
+    constraint JOUER_C2_FK1 foreign key (NomFilm) references FILM (NomFilm),
+    constraint JOUER_C3_FK2 foreign key (NomActeur) references ACTEUR (NomActeur)
 );
 
 CREATE TABLE CATEGORIE (
-    IdFilm number(4),
-    IdGenre number(4),
-    constraint CATEGORIE_C1_PK primary key (IdFilm, IdGenre),
-    constraint CATEGORIE_C2_FK1 foreign key (IdFilm) references FILM (IdFilm),
-    constraint CATEGORIE_C3_FK2 foreign key (IdGenre) references GENRE (IdGenre)
+    NomFilm varchar2(100),
+    NomGenre varchar2(100),
+    constraint CATEGORIE_C1_PK primary key (NomFilm, NomGenre),
+    constraint CATEGORIE_C2_FK1 foreign key (NomFilm) references FILM (NomFilm)
 );
 
 CREATE TABLE CARTE_BANCAIRE (
-    IdCarteBancaire number(4),
     NumeroCarteBancaire varchar2(17),
     ExpirationCarteBancaire date,
     PictogrammeCarteBancaire varchar2(4),
-    constraint CARTE_BANCAIRE_C1_PK primary key (IdCarteBancaire)
+    AdresseMailClient varchar2(100) not null,
+    constraint CARTE_BANCAIRE_C1_PK primary key (NumeroCarteBancaire, ExpirationCarteBancaire, PictogrammeCarteBancaire),
+    constraint CARTE_BANCAIRE_C2_FK1 foreign key (AdresseMailClient) references CLIENT (AdresseMailClient)
 );
 
 CREATE TABLE CLIENT (
-	IdClient number(4),
+    AdresseMailClient varchar2(100) not null,
 	NomClient varchar2(100) not null,
 	PrenomClient varchar2(100) not null,
-    AdresseMailClient varchar2(100) not null,
     AdressePostalClient varchar2(100),
     DateNaissanceClient date,
     NumeroTelephoneClient varchar2(11),
     MotDePasseClient varchar2(100) not null,
-    IdCarteBancaire number(4),
-	constraint CLIENT_C1_PK primary key (IdClient),
-    constraint CLIENT_C2_FK1 foreign key (IdCarteBancaire) references CARTE_BANCAIRE (IdCarteBancaire)
+	constraint CLIENT_C1_PK primary key (AdresseMailClient)
 );
 
 CREATE TABLE SOUHAIT (
-    IdFilm number(4),
-    IdClient number(4),
-    constraint JOUER_C1_PK primary key (IdFilm, IdClient),
-    constraint JOUER_C2_FK1 foreign key (IdFilm) references FILM (IdFilm),
-    constraint JOUER_C3_FK2 foreign key (IdClient) references CLIENT (IdClient)
+    NomFilm varchar2(100),
+    AdresseMailClient varchar2(100),
+    constraint SOUHAIT_C1_PK primary key (NomFilm, AdresseMailClient),
+    constraint SOUHAIT_C2_FK1 foreign key (NomFilm) references FILM (NomFilm),
+    constraint SOUHAIT_C3_FK2 foreign key (AdresseMailClient) references CLIENT (AdresseMailClient)
 );
 
 CREATE TABLE TARIF (
-    IdTarif number(4),
-    TarifClientLambda number(4),
-    TarifClientAbonne number(4),
-    constraint EMPRUNT_C1_PK primary key (IdTarif)
+    NomTarif varchar2(100),
+    PrixTarif number(4),
+    constraint TARIF_C1_PK primary key (NomTarif)
 );
 
 CREATE TABLE EMPRUNT (
-    IdEmprunt number(4),
     DateDebutEmprunt date not null,
     TypeEmprunt varchar2(100) not null,
-    IdFilm number(4),
-    IdClient number(4),
-    IdTarif number(4),
-    constraint EMPRUNT_C1_PK primary key (IdEmprunt),
-    constraint EMPRUNT_C2_FK1 foreign key (IdFilm) references FILM (IdFilm),
-    constraint EMPRUNT_C3_FK2 foreign key (IdClient) references CLIENT (IdClient),
-    constraint EMPRUNT_C4_FK3 foreign key (IdTarif) references TARIF (IdTarif)
+    NomFilm varchar2(100),
+    AdresseMailClient varchar2(100),
+    NomTarif varchar2(100),
+    constraint EMPRUNT_C1_PK primary key (DateDebutEmprunt, NomFilm, AdresseMailClient),
+    constraint EMPRUNT_C2_FK1 foreign key (NomFilm) references FILM (NomFilm),
+    constraint EMPRUNT_C3_FK2 foreign key (AdresseMailClient) references CLIENT (AdresseMailClient),
+    constraint EMPRUNT_C4_FK3 foreign key (NomTarif) references TARIF (NomTarif)
 );
 
 CREATE TABLE CARTE_ABONNEMENT (
-    IdCarteAbonnement number(4),
+    NomCarteAbonnement varchar2(100),
     SoldeCarteAbonnement number(4),
     AgePropietaireCarteAbonnement number(4),
-    IdClient number(4),
-    constraint CARTE_ABONNEMENT_C1_PK primary key (IdCarteAbonnement),
-    constraint CARTE_ABONNEMENT_C2_FK2 foreign key (IdClient) references CLIENT (IdClient)
+    AdresseMailClient varchar2(100),
+    constraint CARTE_ABONNEMENT_C1_PK primary key (NomCarteAbonnement, AdresseMailClient),
+    constraint CARTE_ABONNEMENT_C2_FK2 foreign key (AdresseMailClient) references CLIENT (AdresseMailClient)
 );
 
 CREATE TABLE HISTORIQUE (
-    IdHistorique number(4),
-    DureeEmprunt time not null,
-    NomRealisateur varchar2(100) not null,
-    IdFilm number(4),
-    IdClient number(4),
-    constraint HISTORIQUE_C1_PK primary key (IdHistorique),
-    constraint HISTORIQUE_C2_FK1 foreign key (IdFilm) references FILM (IdFilm),
-    constraint HISTORIQUE_C3_FK2 foreign key (IdClient) references CLIENT (IdClient)
+    DateDebutEmprunt date not null,
+    DateFinEmprunt date not null,
+    TypeEmprunt varchar2(100) not null,
+    NomFilm varchar2(100),
+    AdresseMailClient varchar2(100),
+    constraint HISTORIQUE_C1_PK primary key (DateDebutEmprunt, NomFilm, AdresseMailClient),
+    constraint HISTORIQUE_C2_FK1 foreign key (NomFilm) references FILM (NomFilm),
+    constraint HISTORIQUE_C3_FK2 foreign key (AdresseMailClient) references CLIENT (AdresseMailClient)
 );
