41d8a735ed1810ad1803af239de930039b6796be
Paul Faure-Jeunot <61384536+marship@users.noreply.github.com>
2022-11-10 16:38:19 +0100

    Update Client.java

--- src/modele/Client.java
+++ src/modele/Client.java
@@ -7,11 +7,31 @@ public class Client extends Employe {
 
   private Set FilmsLoues = new HashSet();
 
+  private Set CarteAbo = new HashSet();
+
+  private int nbCartes;
+
   public void addFilm(Film f) {
     FilmsLoues.add(f);
   }
 
   public Set getFilmsLoues() {
     return FilmsLoues;
+  }
+
+  public void addCarte(Carte c) {
+    CarteAbo.add(c);
+  }
+
+  public Set getCarteAbo() {
+    return CarteAbo;
+  }
+
+  public int getnbCarte(){
+    return nbCartes;
+  }
+
+  public void setnbCarte(int n){
+    nbCartes = n;
   }
 }
